ab5b144fbf9a2993ab866bb64f9472593545f909
ruansheng <825307513@qq.com>
2017-06-30 22:01:08 +0800

    zokcat

--- background.js
+++ background.js
@@ -15,12 +15,16 @@ chrome.runtime.onInstalled.addListener(function (object)
 chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
 	if(request.cmd == 'start') {
 		// 调接口获取一个商品id
-		
-		// 打开一个标签页 并且 
-		chrome.tabs.create({ url: 'http://v3.bootcss.com/css/#forms' },function(tab) {
-			chrome.tabs.executeScript(tab.id, {file: "execute_script.js"});
+
+		var detail_url = "https://detail.tmall.com/item.htm?id=550773197674&spm=a223v.7835278.t0.2.469d35e6XYWk61&pvid=86d58fdb-e5e6-43b7-b0f3-9b6a633de30e&scm=1007.12144.81309.9011_8949";
+
+		// 打开一个标签页 并且 注入js代码
+		chrome.tabs.create({ url: detail_url },function(tab) {
+			//chrome.tabs.executeScript(tab.id, {file: "execute_script.js"});
 		});
 
+	} else if(request.cmd == 'data') {
+		alert(request.data);
 	}
 
 });
--- content.js
+++ content.js
@@ -1,7 +1,13 @@
 ﻿
 function start(){
 	chrome.extension.sendMessage({cmd: "start"}, function (response) {
-		;
+
+	});
+}
+
+function sendData(data){
+	chrome.extension.sendMessage({cmd: "data", data: data}, function (response) {
+
 	});
 }
 
@@ -9,4 +15,39 @@ $(function(){
 	$('#start-run').click(function() {
 		start();
 	});
+
+	if(window.location.host == 'detail.tmall.com') {
+		// 月销量
+		var yxl = $('.tm-ind-sellCount').find('.tm-indcon');
+		var yxl_count = yxl.find('.tm-count').text();
+
+		// 累计评价
+		var ljpj = $('.tm-ind-reviewCount').find('.tm-indcon');
+		var ljpj_count = ljpj.find('.tm-count').text();
+
+		// 送天猫积分
+		var stmjf = $('.tm-ind-emPointCount').find('.tm-indcon');
+		var stmjf_count = stmjf.find('.tm-count').text();
+
+		// 库存
+		var kucun = $('#J_EmStock').text();
+
+		// 价格
+		var price = $('#J_StrPriceModBox').find('.tm-price').text();
+
+		// 促销价
+		var cx_price = $('#J_PromoPrice').find('.tm-price').text();
+
+		//组装数据发送到 background
+		var data = {
+			yxl_count:yxl_count,
+			ljpj_count:ljpj_count,
+			stmjf_count:stmjf_count,
+			kucun:kucun,
+			price:price,
+			cx_price:cx_price
+		};
+		sendData(data);
+	}
+
 });
